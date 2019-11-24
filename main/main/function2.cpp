37c9e7e6c44169ee48472be02f8740818829c791
Vladislav <vladislav070599@rambler.ru>
2019-11-25 00:56:22 +0300

    add function addition and multiplication

---
+++ main/main/function2.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h" 
+double funcadd(double a, double b) 
+{ 
+return a+b; 
+} 
+double funcmulti(double a, double b) 
+{ 
+return a*b; 
+}
---
+++ main/main/function2.h
@@ -0,0 +1,5 @@
+#ifndef __FUNCTION2_H__ 
+#define __FUNCTION2_H__ 
+int funcadd(double,double); 
+int funcmulti(double,double); 
+#endif
