5ab2e52c5d80f2748c1b1387a9c4c229eb29356b
Yaroslav Zhgilev <jarslavzhgilev@rambler.ru>
2019-11-25 03:08:38 +0300

    modification main

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -3,39 +3,40 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "function1.h"
 using namespace std;
 
 int main(int argc, char* argv[])
 	{
 		 int count; // переменная для выбора в switch
 		 double a, b; // переменные для хранения операндов
-			cout << "Vvedite pervoe chislo: ";
+			cout << "Vvedite pervoe chislo: "<<endl;
 			cin >> a;
-			cout << "Vvedite vtoroe chislo: ";
+			cout << "Vvedite vtoroe chislo: "<<endl;
 			cin >> b;
-			cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: ";
+			cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: "<<endl;
 			 cin >> count;
  switch (count) // начало оператора switch
  {
 
 case 1: // если count = 1
  {
-	 cout<<"vremenno nedostupno"<<endl;
+	 cout<<funcadd(a,b)<<endl;
  break;
  }
  case 2: // если count = 2
  {
-	 cout<<"vremenno nedostupno"<<endl;
+	 cout<<funcsub(a,b)<<endl;
  break;
  }
  case 3: // если count = 3
  {
-	 cout<<"vremenno nedostupno"<<endl;
+	 cout<<funcmulti(a,b)<<endl;
  break;
  }
  case 4: // если count = 4
  {
-	 cout<<"vremenno nedostupno"<<endl;
+	 cout<<funcdiv(a,b)<<endl;
  break;
  }
  default: // если count равно любому другому значению
